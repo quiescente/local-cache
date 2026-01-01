86f9bcf5e9c591fdd9d89948620d54a6daa59845
Thiago Oliveira <thiago@signaes.com>
2026-01-01 18:11:57 -0300

    Add comments to the main class

--- main.ts
+++ main.ts
@@ -1,18 +1,37 @@
 import { Database } from "./db.ts";
-import type { SerializablePersistenceLayer } from "./types.ts";
+import type {
+  PersistenceLayer,
+  SerializablePersistenceLayer,
+} from "./types.ts";
 
+/**
+ * The LocalCache class implements the SerializablePersistenceLayer by exposing a set and a get method
+ */
 export class LocalCache implements SerializablePersistenceLayer {
+  /* The location where the database should be created or looked up, ":memory:" can be used instead if SQLite is used (which is the default implementation) */
   public location: string;
-  private db: Database;
+  /* The implementation of the PersistenceLayer interface */
+  private db: PersistenceLayer;
 
+  /*
+   * @param location This is where the database file will be created if using SQLite (which is the default), which also accepts ":memory:" so that the database is created in memory and not persisted to a file
+   * @param db A function receiving the location string (from the first param) and returning a implementation of the PersistenceLayer interface
+   */
   constructor(
     location: string,
-    db: (location: string) => Database = (location) => new Database(location),
+    db: (location: string) => PersistenceLayer = (location) =>
+      new Database(location),
   ) {
     this.location = location;
     this.db = db(location);
   }
 
+  /*
+   * A method to set a value in the cache
+   * @param key A string for the cache entry key, which can be used for retrieving its value with the get method
+   * @param value The value to be cached, it should be serializable (stringifiable) by the serialize function
+   * @param serialize A function to serialize the value to a string, defaulting to `JSON.stringify`
+   */
   public set<T>(
     key: string,
     value: T,
@@ -21,6 +40,12 @@ export class LocalCache implements SerializablePersistenceLayer {
     return this.db.set(key, serialize(value));
   }
 
+  /*
+   * A method to get a value from the cache by key
+   * @param key The key that was used to set a value before
+   * @param deserialize A function to deserialize the value from a string to the expected type, defaulting to `JSON.parse`
+   * @returns the deserialized value or undefined if nothing found by the passed in key
+   */
   public get<T>(
     key: string,
     deserialize: (value: string) => T | undefined = JSON.parse,
